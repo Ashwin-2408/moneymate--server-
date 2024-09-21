51c4d2af8c9b343c3e069f23d0011f1444b6908b
AShwin <ashwin.thanga@gmail.com>
2024-09-21 23:56:27 +0530

    started login

--- Controllers/Login_Controller.js
+++ Controllers/Login_Controller.js
@@ -3,11 +3,15 @@ const pool = require("../db_connection");
 const Login_controllers = {
   signup: async (req, res) => {
     const { Email, Password } = req.body;
-    if (!Email | !Password) {
+    if (!Email || !Password) {
       return res.status(400).send({ ERR: "invalid body request" });
     } else {
+      let connection;
       try {
-        const [rows, fields] = await pool.query(
+        connection = await pool.getConnection();
+
+        await connection.beginTransaction();
+        const [rows, columns] = await pool.query(
           "select * from login where Email=? ",
           Email
         );
@@ -16,8 +20,12 @@ const Login_controllers = {
             Email,
             Password,
           ]);
-          return res.status(200).send({ LOG: "Successfully registered" });
+          await connection.commit();
+          return res
+            .status(200)
+            .send({ LOG: "Successfully registered", Message: "Success" });
         } else {
+          await connection.rollback();
           return res.status(403).send({ ERR: "Email is already registered" });
         }
       } catch (error) {
@@ -25,6 +33,26 @@ const Login_controllers = {
       }
     }
   },
+  login: async (req, res) => {
+    const { Email, Password } = req.body;
+    if (!Email || !Password) {
+      res.status(400).send({ ERR: "Invalid body request" });
+    } else {
+      let connection;
+      try {
+        connection = await pool.getConnection();
+        connection.beginTransaction();
+        const [rows, columns] = await pool.query(
+          "select * from login where email=?",
+          [Email]
+        );
+        if (rows.Password != Password) {
+          connection.rollback();
+          res.status(400).send({ ERR: "passowrd is incorrect" });
+        }
+      } catch (error) {}
+    }
+  },
 };
 
-module.exports=Login_controllers
+module.exports = Login_controllers;
--- Routes/Login_Route.js
+++ Routes/Login_Route.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const Login_Controllers = require("../Controllers/Login_Controller");
-router.use("/Signup", (req, res) => {
+router.post("/Signup", (req, res) => {
   Login_Controllers.signup(req, res);
 });
+router.use("/Login",(req,res)=>{
+  Login_Controllers.login(req,res);
+})
 
 module.exports = router;
